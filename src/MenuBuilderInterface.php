2d692354fe7d7dc6a4fc909b6ba0d022dc0bbd3b
Vaalyn <vaalyndev@gmail.com>
2018-10-13 23:22:50 +0200

    First commit

---
+++ src/MenuBuilderInterface.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Vaalyn\MenuBuilderService;
+
+use Slim\Http\Request;
+
+interface MenuBuilderInterface {
+	/**
+	 * @param string $name
+	 * @param array $menuConfig
+	 * @param Request $request
+	 *
+	 * @return MenuInterface
+	 */
+	public function buildMenuFromConfig(string $name, array $menuConfig, Request $request): MenuInterface;
+
+	/**
+	 * @param string $name
+	 * @param array $menuItemConfig
+	 * @param Request $request
+	 *
+	 * @return MenuItemInterface
+	 */
+	public function buildMenuItemFromConfig(string $name, array $menuItemConfig, Request $request): MenuItemInterface;
+}
